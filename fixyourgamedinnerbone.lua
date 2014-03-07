faf745eac82926d11efaa2290dbc78e3de252097
brynjolf <bongomatic@gmail.com>
2014-03-07 06:21:34 +0100

    Update fixyourgamedinnerbone.lua

--- fixyourgamedinnerbone.lua
+++ fixyourgamedinnerbone.lua
@@ -1,4 +1,5 @@
 -- #JustLuaThings
+-- GITHUB PLEASE RECOGNIZE MY LUA ADDICTION
 local file, msg
     repeat
       print "enter a file name:"
@@ -7,3 +8,18 @@ local file, msg
       file, msg = io.open(name, "r")
       if not file then print(msg) end
     until file
+
+
+function newStack ()
+      return {""}   -- starts with an empty string
+    end
+    
+    function addString (stack, s)
+      table.insert(stack, s)    -- push 's' into the the stack
+      for i=table.getn(stack)-1, 1, -1 do
+        if string.len(stack[i]) > string.len(stack[i+1]) then
+          break
+        end
+        stack[i] = stack[i] .. table.remove(stack)
+      end
+    end
